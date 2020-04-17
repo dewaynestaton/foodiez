840e68dc7eb0e4d5adf5bdea6fca818e5b57ba14
dewaynestaton <src.dewayne@gmail.com>
2020-04-16 17:45:28 -0700

    Add fonts, logo, and test different background images

--- dist/main.48b3db1d.js
+++ dist/main.48b3db1d.js
@@ -184,7 +184,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/main.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./../images/background-textured-light.jpg":[["background-textured-light.0ec961ba.jpg","images/background-textured-light.jpg"],"images/background-textured-light.jpg"],"./../images/rice-and-meat.jpg":[["rice-and-meat.cbf4194c.jpg","images/rice-and-meat.jpg"],"images/rice-and-meat.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -212,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64220" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57356" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
